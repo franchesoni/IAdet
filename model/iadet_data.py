6831f3b189662fd26bdd904fda9b4fc4302b8803
franchesoni <marchesoniacland@gmail.com>
2022-11-03 10:59:07 +0100

    reduce batch size

--- model/iadet_data.py
+++ model/iadet_data.py
@@ -27,7 +27,7 @@ test_pipeline = [
         ))
 ]
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=4,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
